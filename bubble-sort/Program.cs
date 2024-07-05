88c7d59cf51834dd3e806ffa4029056e8b502853
augusto <augustosaaude@gmail.com>
2024-07-04 22:28:35 -0300

    Add project files.

---
+++ bubble-sort/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace bubble_sort;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = [1, 2, 3, 4, 5];
+
+        BubbleSort(array);
+
+        Console.WriteLine("Array ordenado:");
+
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+    }
+
+    public static void BubbleSort(int[] array)
+    {
+        int size = array.Length;
+
+        for (int j = 0; j < size - 1; j++)
+        {
+            var changes = 0;
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    (array[i + 1], array[i]) = (array[i], array[i + 1]);
+                    changes++;
+                }
+            }
+
+            if (changes <= 1) break;
+        }
+    }
+}
---
+++ selection-sort/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace selection_sort;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = [7, 5, 1, 8, 3];
+
+        SelectionSort(array);
+
+        Console.WriteLine("Array ordenado:");
+
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+    }
+
+    static void SelectionSort(int[] lista)
+    {
+        int n = lista.Length;
+
+        for (int j = 0; j < n - 1; j++)
+        {
+            int minIndex = j;
+
+            for (int i = j; i < n; i++)
+            {
+                if (lista[i] < lista[minIndex])
+                {
+                    minIndex = i;
+                }
+            }
+
+            if (lista[j] > lista[minIndex])
+            {
+                int aux = lista[j];
+                lista[j] = lista[minIndex];
+                lista[minIndex] = aux;
+            }
+        }
+    }
+}
